754d00754bc50f5652b3a59c9c63ceaceabd52d2
kevinm919 <kevinm719@mindspring.com>
2024-10-16 16:56:18 -0500

    Adding two new practice files hopefully

---
+++ Python practice 2.py
@@ -0,0 +1,9 @@
+# Collect the user's input for the prompt "What is your name?"
+user_name = input("What is your name? ")
+
+# Print the data type of user_name
+print("user_name is type", type(user_name))
+
+# Collect the user's input for the prompt "How old are you?" and convert the string to an integer.
+
+
---
+++ Python practice.py
@@ -0,0 +1,18 @@
+# Create two variables called 'full_name' and 'country_to_visit' w/ strings
+full_name = "Kevin Miller"
+country_to_visit = "Germany"
+
+# Create one variable called 'days' that will hold an integer
+days = "14"
+
+# Create one variable called 'exchange_rate' that will hold a float
+exchange_rate = "1.5"
+
+# Create a variable called 'satisfied' that will hold a Boolean
+satisfied = "True"
+
+# Print out a sentence for each of the variables. Make sure the variables are printed out on the same line as the rest of the sentence.
+print(full_name, "wants to travel to", country_to_visit)
+print(full_name, "wants to stay for", days)
+print("The exchange rate is", exchange_rate, "dollars to Euros")
+print("Was the customer satisfied with the experience?", satisfied)
